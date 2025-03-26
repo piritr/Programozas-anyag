089cb55ccfdeaf6f8aa7567eba52783097bb2595
piritr <162710464+piritr@users.noreply.github.com>
2025-03-26 08:15:44 +0100

    Add files via upload

---
+++ gyak.py
@@ -0,0 +1,50 @@
+'''import random''' '''''
+
+def osszegzes(self):
+    szum=0
+    for j in lista:
+        szum+=j
+    return szum 
+
+lista=[]
+for i in range(100):
+    lista.append(random.randint(1000,5000))
+print(lista)
+print(f"A sorozat összeg: {osszegzes(lista)}\n és átlaga {osszegzes(lista)/len (lista)}")
+print(f"Összeg sajátfüggvénnyel: {sum(lista)}n")''''
+
+# Szélsőérték keresés tétele_:
+#adott egy N elemű sorozat határozzuk meg a sorozat legnagyobb illetve a legkissebb elemű értéket.
+#Van egy max értékunk ai indulaskor ami minding a sor a legelso eleme.
+#
+
+
+'''
+def legnagyobb(sor):
+    mini=0
+    for i in range(1,len(sor))
+        if sor[maxi]>sor[i]:
+            mini=i
+    return mini
+print(f"sor sorozat legnagyobb eleme a {lista[legkis(lista)]} amely a {legkis(lista)}. helyen található")'''
+#Megszamlalas tetele
+#Adott egy N elemu sorozat sorozat elemein értelmezett t tulajdonsag
+#hatarozzuk meg hogy hany t tulajdonsagu elem van a sorozatba
+''''def megszamolparos(sor):
+    parosdarab=0
+    for szam in sor:
+        if szam%2==0:
+            parosdarab+=1   #parosdarab = parosdarab + 1
+    return parosdarab
+print(f"A sorozatban ")'''
+import random
+
+
+
+
+
+
+
+
+
+
